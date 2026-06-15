10cf12b5610747fd58edbf35b90a299f03eb2f38
geantilchristian-dotcom <geantil.christian@gmail.com>
2026-06-15 14:49:35 +0100

    feat: Comment ajouter champ section (communaute|parole)

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   texte:        { type: String, required: true, trim: true, maxlength: 1000 },
   type:         { type: String, enum: ['commentaire', 'suggestion'], default: 'commentaire' },
+  section:      { type: String, enum: ['communaute', 'parole'], default: 'communaute' },
   auteur:       { type: String, default: '', trim: true },
   dateEnvoi:    { type: Date, default: Date.now },
   adminReponse: { type: String, default: '', trim: true },
