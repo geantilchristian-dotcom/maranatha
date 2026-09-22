fcb520e9dcc1e1fac0f30c428630ecb093da195e
geantilchristian-dotcom <geantil.christian@gmail.com>
2026-09-22 14:42:01 +0200

    Fiabiliser CORS localhost et version de deploiement

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.use(
 );
 const PORT = Number(process.env.PORT || 5000);
 const MONGO_URI = process.env.MONGO_URI;
-const VERSION = '20260731-reveil-auto-v3';
+const VERSION = '20260922-cors-v2';
 
 app.disable('x-powered-by');
 app.set('trust proxy', 1);
